480401bd7a263e2aa3c2e3bd134c08aeabe6a069
장선호 <sunho3456@naver.com>
2023-07-16 17:57:20 +0900

    로그인 화면 완료 ,, ???
    로그인 화면 완료 ,, ???

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import damnBreadLogo from "./images/damnBread_logo.png";
+import "./Login.css";
+
 
 function Login() {
-    const [InputID, setInputID] = useState("");
-    const [InputPW, setInputPW] = useState("");
+    const [InputID, setInputID] = useState("");         //로그인 입력창
+    const [InputPW, setInputPW] = useState("");         //비밀번호 입력창
+    const [infoSave, setInfoSave] = useState(false);    //아이디 저장 체크박스
 
     const handleInputID = (e) => {
         setInputID(e.target.value);
@@ -12,6 +16,11 @@ function Login() {
     const handleInputPW = (e) => {
         setInputPW(e.target.value);
     };
+
+    const handleInfoSaveChange = (e) => {
+        setInfoSave(e.target.checked);
+      };
+    
 
     const onClickLogin = () => {
         console.log("click login");
@@ -53,19 +62,49 @@ function Login() {
 
     return(
         <div>
-            <h2>Login</h2>
-            <div>
-                <label htmlFor='input_id'>ID : </label>
-                <input type='text' name='input_id' value={InputID} onChange={handleInputID} />
-            </div>
-            <div>
-                <label htmlFor='input_pw'>PW : </label>
-                <input type='password' name='input_pw' value={InputPW} onChange={handleInputPW} />
-            </div>
-            <div>
-                <button type='button' onClick={onClickLogin}>Login</button>
+
+                <div className="logo-container">
+                        <img src={damnBreadLogo} alt="damnBreadLogo" width="30"/>
+                        <span className="logo-container-1">
+                            <b>로그인</b>
+                        </span>
+                </div>
+                <div className="division-line"></div>
+
+            <div className="login-form">
+                <body>
+                    <div id="root"></div>
+                    <img src={damnBreadLogo} alt="damnBreadLogo" width="120" className="logo-margin"/>
+                    <div>
+                        <input type='text' name='input_id' placeholder="아이디" value={InputID}
+                             onChange={handleInputID} style={{width:"350px", height: "30px", marginTop: "10px", fontSize: "15px"}} />
+                    </div>
+                    <div>
+                        <input type='password' name='input_pw' placeholder="비밀번호" value={InputPW}
+                             onChange={handleInputPW} style={{width:"350px", height: "30px", marginTop: "15px", fontSize: "15px"}} />
+                    </div>
+                    <div>
+                        <button type='button' onClick={onClickLogin} style={{width:"357px", height:"40px",
+                             marginTop:"15px", backgroundColor:"#BF5E49", color:"#FFFFFF", border:"0px"}}>로그인</button>
+                    </div>
+                    <label style={{fontSize: "7px", color: "#7F7F7F"}}><input type="checkbox" name="info-save" value="login-info-save" 
+                         style={{width: "11px", height: "11px", marginTop: "9px", color: "#BF5E49"}} checked={infoSave}
+                         onChange={handleInfoSaveChange}/><b> 아이디 저장</b></label>
+                        
+                    <label style={{fontSize: "7px", color: "#7F7F7F", marginLeft: "153px"}}>아이디 찾기</label>
+                    <label style={{fontSize: "7px", color: "#7F7F7F"}}> | </label>
+                    <label style={{fontSize: "7px", color: "#7F7F7F"}}>비밀번호 찾기</label>
+
+                    <div>
+                        <label style={{fontSize: "7px", color: "#7F7F7F", marginLeft: "314px"}}><b>회원가입</b></label>
+                    </div>
+
+                    <div className="division-line-1"></div>
+                </body>
+                
             </div>
         </div>
+        
     )
 }
  
