59187743f0fb6303701db2ff7c3e8113d8290fb1
장선호 <sunho3456@naver.com>
2023-07-20 14:54:14 +0900

    sss

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -63,52 +63,6 @@ function Login() {
         alert("회원가입창아앙");    //페이지 이동하는 거 아직 못 찾음 ,,,
     }
 
-    // return(
-    //     <div>
-    //             <div className="logo-container">
-    //                     <img src={damnBreadLogo} alt="damnBreadLogo" width="30"/>
-    //                     <span className="logo-container-1">
-    //                         <b>로그인</b>
-    //                     </span>
-    //             </div>
-    //             <div className="division-line"></div>
-
-    //         <div className="login-form">
-    //             <body>
-    //                 <div id="root"></div>
-    //                 <img src={damnBreadLogo} alt="damnBreadLogo" width="120" className="logo-margin"/>
-    //                 <div>
-    //                     <input type='text' name='input_id' placeholder="아이디" value={InputID}
-    //                          onChange={handleInputID} style={{width:"350px", height: "30px", marginTop: "10px", fontSize: "15px", left:"200px", borderRadius: "15px", padding: ".2em"}} />
-    //                 </div>
-    //                 <div>
-    //                     <input type='password' name='input_pw' placeholder="비밀번호" value={InputPW}
-    //                          onChange={handleInputPW} style={{width:"350px", height: "30px", marginTop: "15px", fontSize: "15px", borderRadius: "15px", padding: ".2em"}} />
-    //                 </div>
-    //                 <div>
-    //                     <button type='button' onClick={onClickLogin} style={{width:"357px", height:"40px",
-    //                          marginTop:"15px", backgroundColor:"#BF5E49", color:"#FFFFFF", border:"0px", borderRadius: "15px"}}>로그인</button>
-    //                 </div>
-    //                 <label style={{fontSize: "7px", color: "#7F7F7F"}}><input type="checkbox" name="info-save" value="login-info-save" 
-    //                      style={{width: "11px", height: "11px", marginTop: "9px", color: "#BF5E49"}} checked={infoSave}
-    //                      onChange={handleInfoSaveChange}/><b> 아이디 저장</b></label>
-                        
-    //                 <label style={{fontSize: "7px", color: "#7F7F7F", marginLeft: "153px"}}>아이디 찾기</label>
-    //                 <label style={{fontSize: "7px", color: "#7F7F7F"}}> | </label>
-    //                 <label style={{fontSize: "7px", color: "#7F7F7F"}}>비밀번호 찾기</label>
-
-    //                 <div>
-    //                     <label onClick={onClickSignUP} style={{fontSize: "7px", color: "#7F7F7F", marginLeft: "314px"}}><b>회원가입</b></label>
-    //                 </div>
-
-    //                 <div className="division-line-1"></div>
-    //             </body>
-                
-    //         </div>
-    //     </div>
-        
-    // )
-
     return(
         <div>
                 <div className="logo-container">
@@ -124,18 +78,20 @@ function Login() {
                     <div id="root"></div>
                     <img src={damnBreadLogo} alt="damnBreadLogo" width="120" className="logo-margin"/>
                     <div>
-                        <input type='text' name='input_id' placeholder="아이디" style={{width:"350px", height: "30px", marginTop: "10px", fontSize: "15px", left:"200px", borderRadius: "15px", padding: ".2em"}} />
+                        <input type='text' name='input_id' placeholder="아이디" value={InputID}
+                             onChange={handleInputID} style={{width:"350px", height: "30px", marginTop: "10px", fontSize: "15px", left:"200px", borderRadius: "15px", padding: ".2em"}} />
                     </div>
                     <div>
-                        <input type='password' name='input_pw' placeholder="비밀번호" style={{width:"350px", height: "30px", marginTop: "15px", fontSize: "15px", borderRadius: "15px", padding: ".2em"}} />
+                        <input type='password' name='input_pw' placeholder="비밀번호" value={InputPW}
+                             onChange={handleInputPW} style={{width:"350px", height: "30px", marginTop: "15px", fontSize: "15px", borderRadius: "15px", padding: ".2em"}} />
                     </div>
                     <div>
-                        <button type='button' style={{width:"357px", height:"40px",
+                        <button type='button' onClick={onClickLogin} style={{width:"357px", height:"40px",
                              marginTop:"15px", backgroundColor:"#BF5E49", color:"#FFFFFF", border:"0px", borderRadius: "15px"}}>로그인</button>
                     </div>
                     <label style={{fontSize: "7px", color: "#7F7F7F"}}><input type="checkbox" name="info-save" value="login-info-save" 
                          style={{width: "11px", height: "11px", marginTop: "9px", color: "#BF5E49"}} checked={infoSave}
-                         /><b> 아이디 저장</b></label>
+                         onChange={handleInfoSaveChange}/><b> 아이디 저장</b></label>
                         
                     <label style={{fontSize: "7px", color: "#7F7F7F", marginLeft: "153px"}}>아이디 찾기</label>
                     <label style={{fontSize: "7px", color: "#7F7F7F"}}> | </label>
@@ -152,8 +108,6 @@ function Login() {
         </div>
         
     )
-
-
 }
  
 export default Login;
