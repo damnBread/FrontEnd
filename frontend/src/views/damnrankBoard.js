016ca5b600fe8180e5f8c3efd92d84e527ec0a0e
장선호 <sunho3456@naver.com>
2023-08-29 13:51:55 +0900

    인재정보 아이템 클릭 시 모달창 뜨게 하는 중

--- frontend/src/views/damnrankBoard.js
+++ frontend/src/views/damnrankBoard.js
@@ -3,6 +3,8 @@ import "../assets/css/damnrankBoard.css";
 import rank from "../assets/img/damnRank.png";
 import man from "../assets/img/damnRank-man-icon.png";
 import woman from "../assets/img/damnRank-woman-icon.png";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 const DamnrankBoard = () => {
     // 더미
@@ -12,6 +14,14 @@ const DamnrankBoard = () => {
       { id: 3, name: "손O산", gender: "남", age: 25, career: "1년 8개월", badge: "따뜻한 인재", title: "적응이 빠른 훌륭한 인재입니다.", workjob: '판매', address: "서울시 노원구"},
       { id: 4, name: "오O아", gender: "여", age: 23, career: "2년 5개월", badge: "적극 응답", title: "지각을 해본 적 없는 인재입니다.", workjob: '서빙, 카페', address: "서울시 노원구"}
     ];
+
+    const [show, setShow] = useState(false);   //모달창
+
+    const handleClose = () => {
+      setShow(false);   //모달창 닫기
+  }
+
+  const handleShow = () =>{ setShow(true)};     //모달창 켜기
 
     const itemsPerPage = 10; // Number of items to display per page
     const totalPages = Math.ceil(rankBoardData.length / itemsPerPage); // Calculate total pages
@@ -56,7 +66,7 @@ const DamnrankBoard = () => {
         </div>
 
         {getCurrentPageItems().map((item) => (
-          <div key={item.id} className="default-filter-rank11">
+          <div key={item.id} className="default-filter-rank11" onClick={handleShow}>
             <div>
                 <span className="label-style111" style={{marginLeft: "30px"}}>
                   <label className="label-style111" style={{zIndex: 1}}><b>경력</b></label>
@@ -73,6 +83,36 @@ const DamnrankBoard = () => {
                   
                     <span className="name-style">
                       <b>{item.name}</b>
+
+                      <Modal dialogClassName="modal-whole-rank" show={show} onHide={handleClose}>
+                                <div>
+                                    <Modal.Body>
+
+                                        {/* 시/도 */}
+                                        <div className="scrollable-container">
+                                          {rankBoardData
+                                            .filter((rankItem) => rankItem.id === item.id)
+                                            .map((item, index) => (
+                                              <div
+                                                  key={index}
+                                                  className={`custom-modal-box`}
+                                              >
+                                                  {item.id}
+                                              </div>
+                                          ))}
+                                        </div>
+                                    </Modal.Body>
+
+                                  </div>
+                                <Modal.Footer>
+                                    <Button className="footer-style" varient="primary">
+                                        채팅하기
+                                    </Button>
+                                    <Button className="footer-style" varient="primary">
+                                        공고 전달하기
+                                    </Button>
+                                </Modal.Footer>
+                            </Modal>
 
 
                       <span className="gender-age">
